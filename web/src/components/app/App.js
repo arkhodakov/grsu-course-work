c29fbaab1f4ef883f4affed2b3d3b5917037a68f
Artem Khodakov <artem.khodakov@exposit.com>
2020-04-02 10:28:01 +0300

    ENH: [Web] React components dir. structure changes

---
+++ web/src/components/app/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./App.css";
+
+import Sidebar from "../sidebar/Sidebar";
+
+function App() {
+  return (
+    <div id="wrapper">
+      <Sidebar />
+    </div>
+  );
+}
+
+export default App;
