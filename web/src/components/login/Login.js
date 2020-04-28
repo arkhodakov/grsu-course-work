13c4cc4b80c81d9cbd0ccedabb288fb684e5249d
Artem Khodakov <altair999z@gmail.com>
2020-04-28 19:28:41 +0300

    ENH: [Web] Login/Registration validation added

--- web/src/components/login/Login.js
+++ web/src/components/login/Login.js
@@ -7,8 +7,47 @@ export default class Login extends React.Component {
   constructor(props) {
     super(props);
 
-    this.state = {};
+    this.state = {
+      email: "",
+      password: "",
+      validated: false,
+    };
+
+    this.handleSubmit = this.handleSubmit.bind(this);
+    this.handleChange = this.handleChange.bind(this);
   }
+
+  handleChange = (e) => {
+    const name = e.target.name;
+    const value = e.target.value;
+    const valid = e.target.checkValidity();
+
+    if (value !== null && value !== "") {
+      e.target.classList.remove("valid", "invalid");
+      e.target.classList.add(valid ? "valid" : "invalid");
+    } else {
+      e.target.classList.remove("valid", "invalid");
+    }
+
+    this.setState({
+      [name]: value,
+    });
+
+    var form = document.getElementById("login-form");
+    var submitButton = document.getElementById("submitButton");
+    submitButton.disabled = !form.checkValidity();
+  };
+
+  handleSubmit = (e) => {
+    e.preventDefault();
+
+    if (!e.target.checkValidity()) {
+      return;
+    } else {
+      const email = this.state.email.value;
+      const password = this.state.password.value;
+    }
+  };
 
   render() {
     return (
@@ -22,27 +61,40 @@ export default class Login extends React.Component {
                   <div class="col-lg-6">
                     <div class="p-5">
                       <div class="text-center">
-                        <h1 class="h4 text-gray-900 mb-4">Husky Bug Tracker</h1>
+                        <h1 class="h4 text-gray-900 mb-4">Bug Tracker</h1>
                         <h1 class="h6 text-gray-900 mb-4">
                           Development footage
                         </h1>
                       </div>
-                      <form class="user">
+                      <form
+                        id="login-form"
+                        class="user"
+                        onSubmit={this.handleSubmit}
+                        noValidate
+                      >
                         <div class="form-group">
                           <input
                             type="email"
+                            name="email"
                             class="form-control form-control-user"
                             id="inputEmail"
                             aria-describedby="emailHelp"
                             placeholder="Email"
+                            value={this.state.email}
+                            onChange={this.handleChange}
+                            required
                           />
                         </div>
                         <div class="form-group">
                           <input
                             type="password"
+                            name="password"
                             class="form-control form-control-user"
                             id="inputPassword"
                             placeholder="Password"
+                            value={this.state.password}
+                            onChange={this.handleChange}
+                            required
                           />
                         </div>
                         <div class="form-group">
@@ -50,22 +102,24 @@ export default class Login extends React.Component {
                             <input
                               type="checkbox"
                               class="custom-control-input"
-                              id="customCheck"
+                              id="rememberMeCheck"
                             />
                             <label
                               class="custom-control-label"
-                              for="customCheck"
+                              for="rememberMeCheck"
                             >
                               Remember Me
                             </label>
                           </div>
                         </div>
-                        <Link
-                          to="/dashboard"
-                          class="btn btn-primary btn-user btn-block"
+                        <button
+                          id="submitButton"
+                          type="submit"
+                          className="btn btn-primary btn-user btn-block"
+                          disabled
                         >
                           Login
-                        </Link>
+                        </button>
                       </form>
                       <hr />
                       <div class="text-center">
--- web/src/components/register/Register.js
+++ web/src/components/register/Register.js
@@ -1,9 +1,49 @@
 import React from "react";
-import { Link } from 'react-router-dom';
+import { Link } from "react-router-dom";
 
 import "./Register.css";
 
 export default class Register extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      email: "",
+      password: "",
+      passwordCheck: "",
+      firstName: "",
+      lastName: "",
+    };
+
+    this.handleSubmit = this.handleSubmit.bind(this);
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  handleChange = (e) => {
+    const name = e.target.name;
+    const value = e.target.value;
+    const valid = e.target.checkValidity();
+
+    if (value !== null && value !== "") {
+      e.target.classList.remove("valid", "invalid");
+      e.target.classList.add(valid ? "valid" : "invalid");
+    } else {
+      e.target.classList.remove("valid", "invalid");
+    }
+
+    this.setState({
+      [name]: value
+    });
+
+    var form = document.getElementById("register-form")
+    var submitButton = document.getElementById("submitButton");
+    submitButton.disabled = !form.checkValidity()
+  };
+
+  handleSubmit = (e) => {
+    e.preventDefault();
+  };
+
   render() {
     return (
       <div class="container">
@@ -16,57 +56,77 @@ export default class Register extends React.Component {
                   <div class="text-center">
                     <h1 class="h4 text-gray-900 mb-4">Create an Account!</h1>
                   </div>
-                  <form class="user">
+                  <form id="register-form" class="user" onSubmit={this.handleSubmit} noValidate>
                     <div class="form-group row">
                       <div class="col-sm-6 mb-3 mb-sm-0">
                         <input
                           type="text"
+                          name="firstName"
                           class="form-control form-control-user"
                           id="firstName"
                           placeholder="First Name"
+                          value={this.state.firstName}
+                          onChange={this.handleChange}
                         />
                       </div>
                       <div class="col-sm-6">
                         <input
                           type="text"
+                          name="lastName"
                           class="form-control form-control-user"
                           id="lastName"
                           placeholder="Last Name"
+                          value={this.state.lastName}
+                          onChange={this.handleChange}
                         />
                       </div>
                     </div>
                     <div class="form-group">
                       <input
                         type="email"
+                        name="email"
                         class="form-control form-control-user"
                         id="inputEmail"
                         placeholder="Email Address"
+                        value={this.state.email}
+                        onChange={this.handleChange}
+                        required
                       />
                     </div>
                     <div class="form-group row">
                       <div class="col-sm-6 mb-3 mb-sm-0">
                         <input
                           type="password"
+                          name="password"
                           class="form-control form-control-user"
                           id="inputPassword"
                           placeholder="Password"
+                          value={this.state.password}
+                          onChange={this.handleChange}
+                          required
                         />
                       </div>
                       <div class="col-sm-6">
                         <input
                           type="password"
+                          name="passwordCheck"
                           class="form-control form-control-user"
-                          id="repeatPassword"
+                          id="passwordCheck"
                           placeholder="Repeat Password"
+                          value={this.state.passwordCheck}
+                          onChange={this.handleChange}
+                          required
                         />
                       </div>
                     </div>
-                    <Link
-                      to="/"
+                    <button
+                      id="submitButton"
+                      type="submit"
                       class="btn btn-primary btn-user btn-block"
+                      disabled
                     >
                       Register Account
-                    </Link>
+                    </button>
                   </form>
                   <hr />
                   <div class="text-center">
